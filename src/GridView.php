32288e93670f7651b81b98845e9fff783fc0c24e
marco <marco@vlad>
2016-12-16 16:28:14 +0100

    fixed php notice errors

--- src/GridView.php
+++ src/GridView.php
@@ -1,133 +1,260 @@
 <?php
 /*
- * HiPanel core package
+ * Advanced Grid for Yii2
  *
- * @link      https://hipanel.com/
- * @package   hipanel-core
+ * @link      https://github.com/hiqdev/yii2-higrid
+ * @package   yii2-higrid
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2014-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
  */
 
-namespace hal\tech\grid;
+namespace hiqdev\higrid;
 
-use yii\helpers\ArrayHelper;
-use hal\tech\widgets\LinkSorter;
-use hiqdev\assets\datatables\DataTablesAsset;
-use hiqdev\assets\icheck\iCheckAsset;
+use Closure;
+use hiqdev\yii2\assets\JqueryResizableColumns\ResizableColumnsAsset;
+use ReflectionClass;
 use Yii;
+use yii\data\ArrayDataProvider;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
+use yii\web\JsExpression;
 
 /**
  * Class GridView.
- * HiPanel specific GridView.
+ *
+ * Gives 2 features:
+ * - creates DetailView widget based on this GridView
+ * - default columns functionality
+ * - representations functionality
+ * - summary section extendability with summaryRenderer()
  */
-class GridView extends \hiqdev\higrid\GridView
+class GridView extends \yii\grid\GridView
 {
-    public $sorter = [
-        'class' => LinkSorter::class,
-    ];
+    public $boxed = true;
     /**
      * {@inheritdoc}
      */
-    public $resizableColumns = [
-        'resizeFromBody' => false,
-    ];
+    public $dataColumnClass = DataColumn::class;
     /**
      * {@inheritdoc}
      */
-//    public $tableOptions = [
-//        'class' => 'table table-bordered table-hover dataTable'
-//    ];
+    public static $detailViewClass = DetailView::class;
     /**
-     * {@inheritdoc}
+     * @var array|boolean
+     *  - array: options for Jquery Resizable Columns plugin initiate call
+     *  - boolean false: resizable is disabled
+     *
+     * Defaults to `['store' => new JsExpression('store')]`.
+     * @see registerResizableColumns()
      */
-    public $options = [
-        'class' => 'dataTables_wrapper form-inline',
-        'role' => 'grid',
-    ];
-    /**
-     * {@inheritdoc}
-     */
-    public $layout = "<div class='dataTables_info'>{summary}</div>\n</div><div class='row'><div class='col-xs-12'>{sorter}</div></div><div class=\"table-responsive\">{items}</div>\n<div class='dataTables_paginate paging_bootstrap'>{pager}</div>";
+    public $resizableColumns = [];
 
-    /**
-     * {@inheritdoc}
-     */
-    protected static function defaultColumns()
-    {
-        return [
-//            'seller_id' => [
-//                'class' => SellerColumn::class,
-//            ],
-//            'client_id' => [
-//                'class' => ClientColumn::class,
-//            ],
-            'checkbox' => [
-                'class' => CheckboxColumn::class,
-            ],
-//            'seller' => [
-//                'class' => SellerColumn::class,
-//                'attribute' => 'seller_id',
-//            ],
-//            'client' => [
-//                'class' => ClientColumn::class,
-//                'attribute' => 'client_id',
-//            ],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function run()
     {
-//        $this->tableOptions['class'] .= ' ' . Yii::$app->themeManager->settings->getCssClass('table_condensed');
-        $this->tableOptions['class'] .= ' ' . 'table_condensed';
+        $this->registerResizableColumns();
         parent::run();
-        $this->registerClientScript();
     }
 
     /**
      * {@inheritdoc}
+     */
+    public function getId($autoGenerate = true)
+    {
+        if ($autoGenerate && parent::getId(false) === null) {
+            $this->id = hash('crc32b', Json::encode($this->columns));
+        }
+        return parent::getId();
+    }
+
+    /**
+     * Registers ResizableColumns plugin when [[resizableColumns]] is not false.
+     */
+    public function registerResizableColumns()
+    {
+        if (!$this->resizableColumns !== false) {
+            return;
+        }
+        $this->tableOptions['data-resizable-columns-id'] = $this->id;
+        ResizableColumnsAsset::register($this->getView());
+        $resizableColumns = Json::encode(ArrayHelper::merge([
+            'store' => new JsExpression('store'),
+        ], $this->resizableColumns));
+        $this->getView()->registerJs("$('#{$this->id} table[data-resizable-columns-id]').resizableColumns($resizableColumns);");
+    }
+
+    /**
+     * Runs DetailView widget based on this GridView.
+     *
+     * @param array $config Config that will be passed to [[detailViewClass]] initialisation.
+     * Special element `gridOptions` will be merged to `GridView` initialisation config array.
+     *
+     * @throws \yii\base\InvalidConfigException
+     *
+     * @return mixed
      */
     public static function detailView(array $config = [])
     {
-        $config = ArrayHelper::merge(['gridOptions' => ['resizableColumns' => ['resizeFromBody' => true]]], $config);
-        return parent::detailView($config);
+        $class = static::$detailViewClass ?: DetailView::class;
+        $grid = Yii::createObject(ArrayHelper::merge([
+            'class' => get_called_class(),
+            'dataProvider' => new ArrayDataProvider(['allModels' => [$config['model']]]),
+        ], ArrayHelper::remove($config, 'gridOptions', [])));
+        $config['grid'] = $grid;
+        return call_user_func([$class, 'widget'], $config);
+    }
+
+    /**
+     * Creates a [[DataColumn]] object with given additional config.
+     * @param array $config additional config for [[DataColumn]]
+     * @return DataColumn the column instance
+     */
+    protected function createColumnObject(array $config = [])
+    {
+        return Yii::createObject(array_merge([
+            'class' => $this->dataColumnClass ?: 'yii\grid\DataColumn',
+            'grid' => $this,
+        ], $config));
+    }
+
+    /**
+     * Default (predefined) columns.
+     * @return array array of predefined DataColumn configs
+     */
+    protected static function defaultColumns()
+    {
+        return [];
+    }
+    /**
+     * @var array Cached default columns.
+     */
+    protected static $_defaultColumns = [];
+
+    /**
+     * Getter for $_defaultColumns.
+     *
+     * @return array
+     */
+    public static function getDefaultColumns()
+    {
+        $class = get_called_class();
+        if (is_array(ArrayHelper::getValue(static::$_defaultColumns, $class))) {
+            return static::$_defaultColumns[$class];
+        }
+        return static::$_defaultColumns[$class] = static::gatherDefaultColumns();
+    }
+
+    /**
+     * Scans recursively by hierarchy for defaultColumns and caches to $_defaultColumns.
+     */
+    public static function gatherDefaultColumns()
+    {
+        $columns = static::defaultColumns();
+        $parent = (new ReflectionClass(get_called_class()))->getParentClass();
+        if ($parent->hasMethod('gatherDefaultColumns')) {
+            $columns = array_merge(call_user_func([$parent->getName(), 'gatherDefaultColumns']), $columns);
+        }
+        return $columns;
+    }
+
+    /**
+     * Returns column from $_defaultColumns.
+     *
+     * @return array|null DataColumn config or null if not found
+     */
+    public static function column($name, array $config = [])
+    {
+        $column = ArrayHelper::getValue(static::getDefaultColumns(), $name);
+        return is_array($column) ? array_merge($column, $config) : null;
     }
 
     /**
      * {@inheritdoc}
      */
-    private function registerClientScript()
+    protected function createDataColumn($text)
     {
-        $view = $this->getView();
-        DataTablesAsset::register($view);
-        iCheckAsset::register($view);
-        $view->registerJs(<<<'JS'
-$(function () {
-    var checkAll = $('input.select-on-check-all');
-    var checkboxes = $('input.icheck');
-    $('input.icheck, input.select-on-check-all ').iCheck({
-        checkboxClass: 'icheckbox_minimal-blue',
-        radioClass: 'iradio_minimal-blue'
-    });
-    checkAll.on('ifChecked ifUnchecked', function(event) {
-        if (event.type == 'ifChecked') {
-            checkboxes.iCheck('check');
-        } else {
-            checkboxes.iCheck('uncheck');
+        $column = static::column($text);
+        if (is_array($column)) {
+            $column['attribute'] = ArrayHelper::getValue($column, 'attribute', $text);
+            return $this->createColumnObject($column);
         }
-    });
-    checkboxes.on('ifChanged', function(event){
-        if (checkboxes.filter(':checked').length == checkboxes.length) {
-            checkAll.prop('checked', true);
-        } else {
-            checkAll.prop('checked', false);
+        return parent::createDataColumn($text);
+    }
+    /**
+     * @var string|Closure use it to change default summary rendering
+     */
+    public $summaryRenderer;
+
+    public function renderSummary()
+    {
+        return $this->summaryRenderer instanceof Closure ? call_user_func($this->summaryRenderer, $this) : (parent::renderSummary() . $this->summaryRenderer);
+    }
+
+    public function parentSummary()
+    {
+        return parent::renderSummary();
+    }
+    /**
+     * @var string selected representation
+     */
+    protected $_representation;
+    /**
+     * @var array available representations
+     */
+    protected static $_representations = [];
+
+    /**
+     * Default (predefined) representations.
+     * Representation is array of label and columns like this:
+     * TODO add Representation class
+     * [
+     *     'label' => Yii::t('cat', 'common'),
+     *     'columns' => ['id', 'name', ... ],
+     * ].
+     * @return array array of predefined representations
+     */
+    public static function defaultRepresentations()
+    {
+        return [];
+    }
+
+    /**
+     * Returns representations.
+     * If not set gets from defaultRepresentations().
+     * @static
+     * @return array
+     */
+    public static function getRepresentations()
+    {
+        $class = get_called_class();
+        if (!isset(static::$_representations[$class])) {
+            static::$_representations[$class] = array_filter(static::defaultRepresentations());
         }
-        checkAll.iCheck('update');
-    });
-});
-JS
-        , \yii\web\View::POS_READY);
+        return static::$_representations[$class];
+    }
+
+    /**
+     * Sets current representation.
+     * Also sets columns list.
+     * @param string $value
+     */
+    public function setRepresentation($value)
+    {
+        $representations = static::getRepresentations();
+        if (!isset($representations[$value])) {
+            $value = key($representations);
+        }
+        if ($value) {
+            $this->columns = $representations[$value]['columns'];
+            $this->_representation = $value;
+        }
+    }
+
+    /**
+     * Returns current selected representation.
+     * @return string
+     */
+    public function getRepresentation()
+    {
+        return $this->_representation;
     }
 }
